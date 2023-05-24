764964d6adf393976b919a2ecc6a1f43dc725350
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-05-24 23:36:50 +0300

    rating bar added

--- lib/pages/MoviePage.dart
+++ lib/pages/MoviePage.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 import '../widgets/CustomNavBar.dart';
 import '../widgets/MoviePageButtons.dart';
 
-class MoviePage extends StatelessWidget {
-  const MoviePage({super.key});
+class MoviePage extends StatefulWidget {
+  MoviePage({super.key});
+
+  @override
+  State<MoviePage> createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> {
+  late double _rating;
+
+  double _userRating = 3.0;
+
+  int _ratingBarMode = 1;
+
+  double _initialRating = 2.0;
+
+  bool _isRTLMode = false;
+
+  bool _isVertical = false;
+
+  IconData? _selectedIcon;
 
   @override
   Widget build(BuildContext context) {
@@ -62,21 +82,49 @@ class MoviePage extends StatelessWidget {
                         Container(
                           width: 180,
                           height: 250,
-                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.red, boxShadow: [
-                            BoxShadow(
-                                color: Colors.red, blurRadius: 8, spreadRadius: 1)
-                          ]),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.red,
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.red,
+                                    blurRadius: 8,
+                                    spreadRadius: 1)
+                              ]),
                           child: ClipRect(
-                            child: Image.asset("assets/images/photo1.jpg",width: 180,height: 250,),
+                            child: Image.asset(
+                              "assets/images/photo1.jpg",
+                              width: 180,
+                              height: 250,
+                            ),
                           ),
                         ),
                       ],
                     ),
                   ),
-                   SizedBox(
+                  SizedBox(
                     height: 20,
                   ),
                   MoviePageButtons(),
+                  RatingBar.builder(
+                    initialRating: _initialRating,
+                    minRating: 1,
+                    //direction: _isVertical ? Axis.vertical : Axis.horizontal,
+                    allowHalfRating: true,
+                    unratedColor: Colors.amber.withAlpha(50),
+                    itemCount: 5,
+                    itemSize: 50.0,
+                    itemBuilder: (context, _) => Icon(
+                      _selectedIcon ?? Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: (rating) {
+                      setState(() {
+                        _rating = rating;
+                      });
+                    },
+                    updateOnDrag: true,
+                  )
                 ],
               ),
             ),
